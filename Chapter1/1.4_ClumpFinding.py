f67265d0c9aafd25d51aaf178fd8fe087b27ce63
mmeghan <mmeghan@uab.edu>
2022-02-22 14:22:41 -0600

    chapter1

--- Chapter1/1.4_ClumpFinding.py
+++ Chapter1/1.4_ClumpFinding.py
@@ -1,4 +1,4 @@
-from os import read
+#from os import read
 
 
 def readFile(file):
@@ -27,18 +27,22 @@ def FrequencyTable(txt, k):
 
 def FindClumps(file):
     reader = readFile(file)
+    Genome = reader[0]
+    k = reader[1] 
+    L = reader[2]
+    t = reader[3]
     patterns = []
-    n = len(reader[0])
-    for i in range(0, (n-reader[2])):
-        window = reader[0][i:(reader[2])]
-        freqMap = FrequencyTable(window, reader[1])
+    n = len(Genome )
+    for i in range(0, (n-L)):
+        window = Genome[i:(L)]
+        freqMap = FrequencyTable(window, k)
         for key in freqMap:
-             if freqMap.get(key) >= reader[3] and key not in patterns:
+             if freqMap.get(key) >= t and key not in patterns:
                 patterns.append(key)
-        i = i + reader[2] -1
+        i = i + L -1
     
     listToStr = ' '.join([str(elem) for elem in patterns])
     return(listToStr)
 
-print(FindClumps('dataset_609063_5.txt'))
-#print(FindClumps('./ClumpFinding/inputs/input_3.txt'))
+#print(FindClumps('dataset_609063_5.txt'))
+print(FindClumps('./testcases/ClumpFinding/inputs/sample.txt'))
