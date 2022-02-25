e8bcc5389061b62f49c7ca742586c5760002415d
mmeghan <mmeghan@uab.edu>
2022-02-25 10:25:56 -0600

    chapter 3

--- Chapter3/3.8_kUniversalString.py
+++ Chapter3/3.8_kUniversalString.py
@@ -3,7 +3,95 @@ Code Challenge: Solve the k-Universal Circular String Problem.
 
 Input: An integer k.
 Output: A k-universal circular string.
+
+
+NOTE: Could not solve becuase kept getting weird error "UnboundLocalError: local variable 'start_node' referenced before assignment" 
+	   not sure why this is happening 
 '''
+
+from copy import deepcopy
+import random
+
+
+def FindStart(adj_list):
+    start = {}
+    for one in adj_list:
+        start.setdefault(one, 0)
+        start[one] += len(adj_list[one])
+    end = {}
+    for one in adj_list:
+        for two in adj_list[one]:
+            end.setdefault(two, 0)
+            end[two] += 1
+    for one in end:
+        try: 
+            if start[one] != end[one]:
+                if start[one] > end[one]:
+                    start_node = one
+                if start[one] < end[one]:
+                    end_node = one
+        except KeyError:
+            end_node = one
+
+    for one in start:
+        try:
+            if end[one] != start[one]:
+                if end[one] < start[one]:
+                    start_node = one
+                if end[one] > start[one]:
+                    end_node = one
+        except KeyError:
+            start_node = one
+    return start_node, end_node
+
+def deBrujin(patterns):
+    graph = {}
+    for pattern in patterns:
+        if pattern[:-1] not in graph:
+            graph[pattern[:-1]] = []
+            graph[pattern[:-1]].append(pattern[1:])
+        else:
+            graph[pattern[:-1]].append(pattern[1:])
+    return graph 
+def EulerianPath(adj_list):
+    circut_max = len(adj_list.values())
+    start_node, end_node = FindStart(adj_list)  
+    if end_node not in adj_list.keys():
+        adj_list.setdefault(end_node, [])
+    adj_list[end_node].append(start_node)
+
+    red_adj_list= {}
+    red_adj_list = deepcopy(adj_list)
+
+    start = start_node
+    print('Start Node:', start)
+    curr = start
+
+    stack = []
+    circut = []
+    while len(circut) != circut_max+1:
+        if red_adj_list[curr] != []:
+            stack.append(curr)
+            step = random.choice(red_adj_list[curr])
+            red_adj_list[curr].remove(step)
+            curr = step
+        else:
+            circut.append(curr)
+            curr = stack[len(stack)-1]
+            stack.pop()
+    circut.pop(0)
+
+    path = start + '->'
+    for vertex in circut[::-1]:
+        path += (vertex + '->')
+    return path.strip('->')
+
+def GenomeConstruction(path):
+    genome = path[0]
+    for element in path[1:]:
+        genome += element[-1]
+    return genome
+
 def kUniversal(n):
 	text = ''
 	kmers = []
@@ -13,13 +101,14 @@ def kUniversal(n):
 			diff = n - len(kmer)
 			kmer = ('0' *diff) + kmer
 		kmers.append(kmer)
-
-	dB = deBruijn(kmers)
+	dB = deBrujin(kmers)
 	path = EulerianPath(dB)
 	edges = []
 	for i in range (len(path)-1):
 		edge = path[i] + path[i+1][-1:]
 		edges.append(edge)
-	text = GenomePath(edges)
-	text = text[:-(k-1)]
+	text = GenomeConstruction(edges)
+	text = text[:-(n-1)]
 	return text
+
+print(kUniversal(4))
--- Chapter3/3.9_ReadPairsReconstruction.py
+++ Chapter3/3.9_ReadPairsReconstruction.py
@@ -7,90 +7,105 @@ Output: a string text with(k,d)-mer composition equal to Paired Reads
 ./testcases/PairedStringReconstruction/inputs/textbookExample.txt
 
 ./datasets/dataset_609102_16.txt
+
+def string_reconstruction_from_read_pairs(patterns, d):
+    print('Patterns:', patterns)
+    dB = debruijn_from_read_pairs(patterns)
+    print('dB:', dB)
+    path = eulerian_path_problem(dB)
+    print('Path:', path)
+    sequence = genome_path_problem(path, d)
+    print('Sequence:',sequence)
+
+
+./datasets/dataset_609102_16.txt
+
 '''
 from copy import deepcopy
 import random
-import sys
-sys.setrecursionlimit(10**6)
+import sys; sys.setrecursionlimit(10**6) 
 
-def ReadFile():
-    with open('./testcases/PairedStringReconstruction/inputs/sample.txt', 'r') as f:
-        values = f.readline().strip()
-        k =  50 #int(values[0])
-        d = 200 #int(values[2])
-        data = f.readlines()
-        kdmers = {}
-        for i in data:
-            kdmer1, kdmer2 = i.strip().split('|')
-            key = (kdmer1[:-1], kdmer2[:-1])
-            val = (kdmer1[1:], kdmer2[1:])
-            kdmers.setdefault(key,[]).append(val)
-    return k, d, kdmers 
-
-def Start(adj_list):
-    degrees = {}
-    for k,vn in adj_list.items():
-        if k not in degrees: degrees[k] = 0
-        degrees[k] += len(vn)
-        for v in vn:
-            if v not in degrees: degrees[v] = 0
-            degrees[v] -= 1
-    s = 0
-    t = 0
-    for k , v in degrees.items():
-        if v > 0:
-            s += 1
-            curr = k
-        if v < 0:
-            t += 1
-    return curr
-
-def deBruijn(kdmers):
-    graph = {}
-    for kdmer in kdmers:
-        prefix = (kdmer[0][:-1], kdmer[1][:-1])
-        suffix = (kdmer[0][1:], kdmer[1][1:])
-        if prefix not in graph.keys():
-            graph[prefix] = []
-            graph[prefix].append(suffix)
+def DeBruijn_pairs(reads):
+    
+    Adjacency = {}
+    for read in reads:
+        prefix = (read[0][:-1], read[1][:-1])
+        suffix = (read[0][1:], read[1][1:])
+        if prefix not in Adjacency:
+            Adjacency[prefix] = [suffix]
         else:
-            graph[prefix].append(suffix)
-    return graph 
+            Adjacency[prefix].append(suffix)
+            
+    return Adjacency
 
-def EulerianPath(adj_list, curr, circut, stack):
-    while True:
-        if adj_list[curr]:
+
+def get_source(graph):
+
+    deg = {}    
+    for v in graph:
+        if v not in deg.keys():
+            deg[v] = -len(graph[v])
+        else:
+            deg[v] -= len(graph[v])
+        for u in graph[v]:
+            if u not in deg.keys():
+                deg[u] = 1
+            else:
+                deg[u] += 1
+    for v in deg:
+        if deg[v] == -1:
+            return v 
+    print('graph does not have an eulerian path, no source.')
+
+def dfs(v, graph, eulerian, stack):
+    circut_max = len(graph.values())
+    red_adj_list= {}
+    red_adj_list = deepcopy(graph)
+    curr = v 
+
+    while len(eulerian) != circut_max+1:
+        if red_adj_list[curr] != []:
             stack.append(curr)
-            step = adj_list[curr].pop(0)
+            step = random.choice(red_adj_list[curr])
+            red_adj_list[curr].remove(step)
             curr = step
         else:
-            circut.append(curr)
-            curr = stack.pop()
-        if all([a == [] for a in adj_list.values()]):
-            break
-    circut.append(curr)
-    while stack:
-        circut.append(stack.pop())
-    circut.reverse()
-    return circut
+            eulerian.append(curr)
+            curr = stack[len(stack)-1]
+            stack.pop()
+    eulerian.pop(0)
+    return eulerian
 
-
-def GenomeConstruction(path, k,d):
-    prefix , suffix = path[0][0], path[0][1]
-    for pair in path[1:]:
-        prefix += pair[0][-1]
-        suffix += pair[1][-1]
-    print('prefix', prefix)
-    print('suffix', suffix)
-    seq = prefix + suffix[-(k+d):]
+def reconstruct(eulerian):
+    prefs, suffs = eulerian[0][0], eulerian[0][1]
+    
+    for pair in eulerian[1:]:
+        prefs += pair[0][-1]
+        suffs += pair[1][-1]
+    
+    if prefs[k+d:] == suffs[:-(k+d)]:
+        print('aligned pref + suff strings')
+    seq = prefs + suffs[-(k+d):]
+    
     return seq
 
-k , d, kdmers = ReadFile()
-adj_list = deBruijn(kdmers)
-startNode  = Start(adj_list)
-circut = []
+
+
+f = open('./testcases/PairedStringReconstruction/inputs/sample.txt', 'r')
+k, d  = (int(i) for i in f.readline().strip().split(' '))
+reads = []
+reads = []
+reads = list(tuple(i.strip('()').strip().split('|')) for i in f.readlines())
+print(reads)
+
+graph = DeBruijn_pairs(reads)
+print('graph:',graph)
+source = get_source(graph)
+print('source:',source)
+eulerian = []
 stack = []
-path = EulerianPath(adj_list, startNode, circut, stack)
-print(path)
-sequence = GenomeConstruction(path, k, d)
-print('sequence:', sequence)
+dfs(source, graph, eulerian, stack)
+print('eulerian:',eulerian)
+eulerian.reverse()
+seq = reconstruct(eulerian)
+print(seq)
